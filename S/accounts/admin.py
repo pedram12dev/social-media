a83ccbc2feb24dab3c0707842fd8fab0219b0e8d
pedram-macbook <pedram@email.com>
2024-01-04 21:01:31 +0330

    create user admin for user

--- S/accounts/admin.py
+++ S/accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    readonly_fields = ('last_login',)
+
+    fieldsets = (
+        ('Main', {'fields': ('phone_number', 'email', 'full_name', 'password')}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('phone_number', 'email', 'full_name', 'password1', 'password2')}),
+    )
+
+    search_fields = ('email', 'full_name')
+    ordering = ('full_name',)
+    filter_horizontal = ()
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj=None, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disable = True
+        return form
+
+
+admin.site.register(User, UserAdmin)
