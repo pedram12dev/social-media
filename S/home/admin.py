93c488f2f156912fd7c1ea2ed0791009b7d1a268
pedram-macbook <pedram@email.com>
2024-01-06 18:14:20 +0330

    add post models and register on admin

--- S/home/admin.py
+++ S/home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user' , 'slug' , 'created')
+    list_filter = ('updated' , )
+    search_fields = ('body' , )
+    prepopulated_fields = {'slug' : ('body' , )}
+    raw_id_fields = ('user' , )
+
+
--- S/home/models.py
+++ S/home/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from accounts.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField()
+    slug = models.SlugField()
+    title = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.title} - {self.slug} - {self.updated}'
+
