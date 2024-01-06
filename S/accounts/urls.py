995594eb8da21f15aea069eeddf3a18005e59567
pedram-macbook <pedram@email.com>
2024-01-06 12:28:14 +0330

    add verify code view and urls and templates

--- S/accounts/urls.py
+++ S/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
+    path('verify/' , views.VerifyCodeView.as_view() , name = 'verify'),
 ]
--- S/accounts/views.py
+++ S/accounts/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, VerifyCodeForm
+from .models import User, OtpCode
+from django.contrib import messages
+from ..utils import send_otp_code
+import random
 
 
 class UserRegisterView(View):
@@ -12,4 +16,43 @@ class UserRegisterView(View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            random_code = random.randint(10000, 99999)
+            send_otp_code(form.cleaned_data['phone'], random_code)
+            OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+            request.session['user_registration_info'] = {
+                'phone_number': form.cleaned_data['phone_number'],
+                'email': form.cleaned_data['email'],
+                'full_name': form.cleaned_data['full_name'],
+                'password': form.cleaned_data['password'],
+            }
+            messages.success(request, 'we send you code', 'success')
+            return redirect('accounts:verify')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserRegisterVerifyCode(View):
+    form_class = VerifyCodeForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'accounts/verify.html', {'form': form})
+
+    def post(self, request):
+        user_session = request.session['user_registration_info']
+        code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == code_instance.code:
+                User.objects.create_user(user_session['phone_number'], user_session['email'], user_session['full_name'],
+                                         user_session['password'])
+                code_instance.delete()
+                messages.success(request, 'you registered successfully', 'success')
+                return redirect('home:home')
+            else:
+                messages.error(request, 'your code is wrong', 'danger')
+                return redirect('accounts:verify')
+
+        return redirect('home:home')
