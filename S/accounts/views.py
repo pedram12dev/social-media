189f252a56ebc30ffb9ab83155882b7d7b4d3736
pedram-macbook <pedram@email.com>
2024-01-06 12:07:26 +0330

    add OtpCode model in accounts app

--- S/accounts/views.py
+++ S/accounts/views.py
@@ -5,7 +5,11 @@ from .forms import UserRegistrationForm
 
 class UserRegisterView(View):
     form_class = UserRegistrationForm
-    def get(self , request):
-        form = UserRegistrationForm()
-        return render(request , 'accounts/register.html' , {'form':form})
+    template_name = 'accounts/register.html'
 
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        pass
